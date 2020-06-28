7b28394f3df2b931e080161dba3df03c4e6afe0a
Calvin Conley <cconley0125@gmail.com>
2020-06-28 14:00:20 -0400

    added lizard and spock

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,13 +1,36 @@
-CHOICES = %w(rock paper scissors)
+CHOICES = %w(rock paper scissors lizard Spock)
+
+RULES = {
+  'rock' => %w(lizard scissors),
+  'lizard' => %w(paper Spock),
+  'scissors' => %w(paper lizard),
+  'Spock' => %w(scissors rock),
+  'paper' => %w(Spock rock)
+}
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+def letter_to_choice(letter)
+  case letter
+  when 'r'
+    'rock'
+  when 'p'
+    'paper'
+  when 's'
+    'scissors'
+  when 'l'
+    'lizard'
+  when 'sp'
+    'Spock'
+  else
+    'invalid'
+  end
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  RULES[first].include?(second)
 end
 
 def display_results(choice, computer_choice)
@@ -20,11 +43,15 @@ def display_results(choice, computer_choice)
   end
 end
 
+computer_score = 0
+player_score = 0
+
 loop do
   choice = ''
   loop do
     prompt("Choose one: #{CHOICES.join(', ')}")
-    choice = gets.chomp
+    prompt("Type the first letter of your choice, or 'sp' for Spock.")
+    choice = letter_to_choice(gets.chomp)
 
     if CHOICES.include?(choice)
       break
@@ -37,11 +64,26 @@ loop do
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
+  if win?(choice, computer_choice)
+    player_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
+
   display_results(choice, computer_choice)
+
+  prompt("Score\n   You: #{player_score}\n   Computer: #{computer_score}")
+
+  if computer_score == 5
+    prompt("The computer is the grand winner!")
+    break
+  elsif player_score == 5
+    prompt("You are the grand winner!")
+  end
 
   prompt("Do you want to play again? ('y' for yes)")
   answer = gets.chomp.downcase
   break unless answer == 'y'
 end
 
-prompt("Thank you for playing rock, paper, scissors.")
+prompt("Thank you for playing rock, paper, scissors, lizard, Spock.")
